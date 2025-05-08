fea6666fd828d55013df9734d31b824b1b201d7d
Sergey Otinov <otinov.prog@gmail.com>
2025-05-08 12:25:54 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -12,5 +12,36 @@ module.exports = {
       defaultLocale: 'en',
       localeDetection: false
    },
-   transpilePackages: ['@mui/x-charts']
+   transpilePackages: ['@mui/x-charts'],
+   async headers() {
+      return [
+         {
+            source: '/sitemap.xml',
+            headers: [
+               {
+                  key: 'Content-Type',
+                  value: 'application/xml',
+               },
+            ],
+         },
+         {
+            source: '/sitemap-:path*.xml',
+            headers: [
+               {
+                  key: 'Content-Type',
+                  value: 'application/xml',
+               },
+            ],
+         },
+         {
+            source: '/robots.txt',
+            headers: [
+               {
+                  key: 'Content-Type',
+                  value: 'text/plain',
+               },
+            ],
+         },
+      ];
+   },
 }

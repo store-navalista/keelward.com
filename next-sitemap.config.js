acdcb0b9a2416fae1a960a185044870d2c64f642
Sergey Otinov <otinov.prog@gmail.com>
2025-05-08 12:21:51 +0300

    fix site

---
+++ next-sitemap.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+   siteUrl: 'https://keelward.com',
+   generateRobotsTxt: true,
+   changefreq: 'daily',
+   priority: 0.7,
+   sitemapSize: 10000,
+   generateIndexSitemap: false,
+   outDir: './public',
+   exclude: ['/privacy-policy', '/404'],
+   alternateRefs: [
+      {
+         href: 'https://keelward.com/',
+         hreflang: 'en',
+      },
+      {
+         href: 'https://keelward.com/ru/',
+         hreflang: 'ru',
+      },
+      {
+         href: 'https://keelward.com/ka/',
+         hreflang: 'ka',
+      },
+      {
+         href: 'https://keelward.com/tr/',
+         hreflang: 'tr',
+      },
+   ],
+   i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'ru', 'ka', 'tr'],
+   },
+}
